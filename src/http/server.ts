bf2045d503d416098b91258b96f8f711348a9f5b
henrique-pignatari <henrique.pignatarimoraes@gmail.com>
2024-02-09 23:58:02 -0300

    feat: add vote on poll

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,19 @@
 import fastify from "fastify"
 import { createPolll } from "./routes/create-poll"
 import { getPoll } from "./routes/get-poll"
+import { voteOnPoll } from "./routes/vote-on-poll"
+import cookie from "@fastify/cookie"
 
 const app = fastify()
 
+app.register(cookie, {
+    secret: "polls-app-nlw",
+    hook: "onRequest",
+})
+
 app.register(createPolll)
 app.register(getPoll)
+app.register(voteOnPoll)
 
 app.listen({port: 3333}).then(() => {
     console.log("HTTP server running!")
